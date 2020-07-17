1c326c0cfaaa3700f76c12a0177ffb7aaa60f654
lewisjackson398 <lewisjackson398@gmail.com>
2020-07-17 21:53:47 +0100

    Updates

---
+++ precache-manifest.8409e15cb30de1329d06936d1a5e5663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b591515ba56f5c205bc2cb792866050",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "177e373946249202f4d6",
+    "url": "/covid-19-tracker/static/css/main.c4695ea0.chunk.css"
+  },
+  {
+    "revision": "4a05d1b57b2a45183856",
+    "url": "/covid-19-tracker/static/js/2.db0c72aa.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.db0c72aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177e373946249202f4d6",
+    "url": "/covid-19-tracker/static/js/main.a615ea7b.chunk.js"
+  },
+  {
+    "revision": "481262977e3783324991",
+    "url": "/covid-19-tracker/static/js/runtime-main.a233e048.js"
+  }
+]);
